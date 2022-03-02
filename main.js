5636055cfd590689984b1b1dcb0e1218b4dfc3e2
wudoO <csereyr@gmail.com>
2022-03-02 13:50:22 +0100

    Switch button to British English counting and Test function added

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 const form = document.getElementById("myForm");
 function handleForm(e) {
     e.preventDefault();
-} 
+}
 form.addEventListener('submit', handleForm);
+
+const checkbox = document.getElementById("flexSwitchCheckDefault");
+checkbox.addEventListener('change', converterFunction);
 
 function converterFunction() {
     const inputNumber = document.getElementById("inputNumber").value;
@@ -14,11 +17,41 @@ function converterFunction() {
 const num = "zero one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen".split(" ");
 const tens = "twenty thirty forty fifty sixty seventy eighty ninety".split(" ");
 
-function numberToWords(iNum){
-    if (iNum < 20) return num[iNum];
-    if (iNum < 100) return tens[~~(iNum/10)-2] + (iNum%10? "-" + num[iNum%10]: "");
-    if (iNum < 1000) return num[~~(iNum/100)] +" hundred" + (iNum%100 == 0? "": " and " + numberToWords(iNum%100));
-    if (iNum < 1000000) return numberToWords(~~(iNum/1000)) + " thousand" + (iNum%1000 != 0? " " + numberToWords(iNum%1000): "");
-    if (iNum < 1000000000) return numberToWords(~~(iNum/1000000)) + " million" + (iNum%1000000 != 0? " " + numberToWords(iNum%1000000): "");
-    if (iNum < 1000000000000) return numberToWords(~~(iNum/1000000000)) + " billion" + (iNum%1000000000 != 0? " " + numberToWords(iNum%1000000000): "");
+function numberToWords(inpNum) {
+    if (inpNum < 20) return num[inpNum];
+    if (inpNum < 100) return tens[~~(inpNum / 10) - 2] + (inpNum % 10 ? "-" + num[inpNum % 10] : "");
+    if (checkbox.checked && inpNum < 10000) {
+        if (inpNum < 10000) return numberToWords(~~(inpNum / 100)) + " hundred" + (inpNum % 100 == 0 ? "" : " and " + numberToWords(inpNum % 100));
+    } else {
+        if (inpNum < 1000) return num[~~(inpNum / 100)] + " hundred" + (inpNum % 100 == 0 ? "" : " and " + numberToWords(inpNum % 100));
+        if (inpNum < 1000000) return numberToWords(~~(inpNum / 1000)) + " thousand" + (inpNum % 1000 != 0 ? " " + numberToWords(inpNum % 1000) : "");
+    }
+    if (inpNum < 1000000000) return numberToWords(~~(inpNum / 1000000)) + " million" + (inpNum % 1000000 != 0 ? " " + numberToWords(inpNum % 1000000) : "");
+    if (inpNum < 1000000000000) return numberToWords(~~(inpNum / 1000000000)) + " billion" + (inpNum % 1000000000 != 0 ? " " + numberToWords(inpNum % 1000000000) : "");
+};
+
+// Test function
+
+function myTest(expectedResult, numberToBeTested) {
+    const resultOfNumberToWords = numberToWords(numberToBeTested);
+    console.log(`Run test: ${(expectedResult == resultOfNumberToWords)}
+    Number to be tested: ${numberToBeTested}
+    Expected: ${expectedResult}
+    Result: ${resultOfNumberToWords}
+    ---------------------------`);
 }
+
+    // Tests - first two should be true, the last two should be false
+    myTest("four hundred and forty-four", 444);
+    myTest("two hundred and twenty-two", 222);
+    myTest("four hundred and forty-four", 44114);
+    myTest("four hundred and forty-four", 444444);
+    // Tests from Github
+    console.log("Oo~~~~~~~~~~~ Examples ~~~~~~~~~~~oO")
+    myTest("seven", 7);
+    myTest("forty-two", 42);
+    myTest("one thousand nine hundred and ninety-nine", 1999);
+    myTest("two thousand and one", 2001);
+    myTest("seventeen thousand nine hundred and ninety-nine", 17999);
+    myTest("three hundred and forty-two thousand two hundred and fifty-one", 342251);
+    myTest("one million three hundred thousand four hundred and twenty", 1300420);
